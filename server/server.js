057a762504a9f4d0580a515bc0064be400aadb6b
Diego Diniz <diegodinizz@gmail.com>
2021-04-21 14:06:04 +0100

    added production statement to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = require('./api')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+  })
+}
 
 app.use('/phonewords/api', router)
 
